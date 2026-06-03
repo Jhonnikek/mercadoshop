7097b9ef9603b516694524f805f5c832750a830f
Jhonnikek <138178283+Jhonnikek@users.noreply.github.com>
2026-06-03 12:18:13 -0500

    eliminar core

--- app/core/context_processors.py
+++
@@ -1,8 +0,0 @@
-from core.models import Carrito
-
-def cart_counter(request):
-    if request.user.is_authenticated and request.user.tipo_usuario == 'CLIENTE':
-        cart = Carrito.objects.filter(cliente=request.user, estado='Activo').first()
-        if cart:
-            return {'cart_items_count': sum(item.cantidad for item in cart.items.all())}
-    return {'cart_items_count': 0}
